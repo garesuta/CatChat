e3ebe74bb8e3920978dcc38810658c7b252e38f8
garesuta <garesuta@gmail.com>
2021-01-28 03:31:47 +0700

    Update action of toolbar in HelpActivity

--- app/src/main/java/com/example/catchat/HelpActivity.java
+++ app/src/main/java/com/example/catchat/HelpActivity.java
@@ -3,6 +3,7 @@ package com.example.catchat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
