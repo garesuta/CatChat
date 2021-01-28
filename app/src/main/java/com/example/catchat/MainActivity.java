e4d50ff0cd9553f9a758b374bcafb7ddb6f599e5
garesuta <garesuta@gmail.com>
2021-01-29 03:15:54 +0700

    Add actions with drawer

--- app/src/main/java/com/example/catchat/MainActivity.java
+++ app/src/main/java/com/example/catchat/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.catchat;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Fragment fragment = new InboxFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.content_frame,fragment);
+        ft.commit();
     }
 }
